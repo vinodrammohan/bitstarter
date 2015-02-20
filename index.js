4c0e1fc20d1111501546c3fbc5616b33bf634a30
Vinod Rammohan <vinodrammohan@gmail.com>
2015-02-20 11:42:59 +0530

    Read from file using readFileSync

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var fs = require('fs');
+var filename = "index.html";
+var buffer = fs.readFileSync(filename, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(buffer.toString('utf-8'))
 })
 
 app.listen(app.get('port'), function() {
